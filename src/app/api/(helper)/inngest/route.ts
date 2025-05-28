0bc53274d6f76c917d334131e1dd69ce5d76699d
Utkarsh Chaudhary <utkarshchaudhary426@gmail.com>
2025-05-28 22:47:54 +0530

    Add inngest dependency and remove unused route file
    This commit introduces the 'inngest' package version 3.38.0 in both package.json and package-lock.json. Additionally, it removes the previously defined route file for 'inngest', which is no longer needed.

--- src/app/api/(helper)/inngest/route.ts
+++
@@ -1,8 +0,0 @@
-import { serve } from 'inngest/next';
-import inngest from '../../../src/inngest/client';
-import { checkAndSendReminders } from '../../../src/inngest/functions/checkAndSendReminders';
-
-export const { GET, POST } = serve({
-  client: inngest,
-  functions: [checkAndSendReminders],
-}); 
